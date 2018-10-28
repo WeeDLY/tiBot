b3a6d9eab911553f5464243458afd10c5f5148e5
Kake <jorgen.sater@gmail.com>
2018-10-28 20:11:38 +0100

    Cleaned up follow_thread. Same person should not occur multple times in the list anymore

--- bot/tiBot/follow_thread.py
+++ bot/tiBot/follow_thread.py
@@ -25,66 +25,84 @@ def run(log, setting, db, twit, imgr):
         log.log(logger.LogLevel.INFO, 'follow_thread.run() sleeping for: %d' % setting.followNewPerson.total_seconds())
         time.sleep(setting.followNewPerson.total_seconds())
 
-        # Find a person
-        q = get_search_q(setting) # Gets a random hashtag as search parameter for finding a follower
-        result = twit.search(q, 100) # Get 100persons I can potentially follow
-        person = None
-        if result is None:
-            log.log(logger.LogLevel.WARNING, 'follow_thread.run: No result when searching \'%s\'' % q)
-        else:
-            personList = get_person_list(result)
-            person = get_person(db, personList, setting)
+        # Follow a person
+        follow(log, setting, db, twit)
 
-        # Follow person
-        if person is None:
-            log.log(logger.LogLevel.WARNING, 'Did not find anyone to follow out of: %d' % len(personList))
-        else:
-            followed = twit.follow_by_name(person.screenName)
-            if followed:
-                q = db.query_commit(query.QUERY_INSERT_FOLLOWS(), (person.screenName, datetime.datetime.now(), 1))
-                if q:
-                    log.log(logger.LogLevel.DEBUG, 'Added %s to database' % person.screenName)
-                else:
-                    log.log(logger.LogLevel.ERROR, 'Failed to add %s to database' % person.screenName)
-            else:
-                log.log(logger.LogLevel.WARNING, 'Failed to follow: %s' % person.to_string())
-        
-        # Unfollow person
-        endDate = datetime.datetime.now() - setting.unfollowPersonAfter
-        personList = db.query_fetchall(query.QUERY_GET_FOLLOWS_UPDATE_QUEUE(), (endDate, ))
-        for person in personList:
-            screenName = person[0]
-            unfollowed = twit.unfollow_by_name(screenName)
-            if unfollowed:
-                unfollow_in_db(log, db, screenName)
-            else:
-                res = twit.get_user_search(screenName)
-                if res is False:
-                    unfollow_in_db(log, db, screenName)
-                else:
-                    log.log(logger.LogLevel.ERROR, 'Failed to unfollow a person that exists: %s' % screenName)
+        # Unfollow person(s)
+        unfollow(log, setting, db, twit)
     # Outside while loop
     log.log(logger.LogLevel.CRITICAL, 'follow_thread.run is not running anymore: %s' % self.setting.runFollowThread)
 
+def follow(log, setting, db, twit):
+    """ Follows a person """
+    q = get_search_q(setting) # Gets a random hashtag as search parameter for finding a follower
+    result = twit.search(q, 100) # Get 100persons I can potentially follow
+    personList = None
+    if result is None:
+        log.log(logger.LogLevel.WARNING, 'follow_thread.run: No result when searching \'%s\'' % q)
+        return
+    else:
+        personList = get_person_list(result)
+    
+    if personList is None:
+        log.log(logger.LogLevel.ERROR, 'Did not find anyone to follow. personList is empty')
+        return
+
+    # Removes all persons, that does not fulfill the requirements set in settings
+    persons = get_valid_persons(personList, setting)
+    # Try to unfollow them 1 by 1, untill 1 is unfollowed.
+    for person in persons:
+        # If person is already in database, skip to next person
+        if db.query_exists(query.QUERY_GET_FOLLOWS(), (person, )):
+            continue
+
+        followed = twit.follow_by_name(person)
+        if followed:
+            q = db.query_commit(query.QUERY_INSERT_FOLLOWS(), (person, datetime.datetime.now(), 1))
+            if q:
+                log.log(logger.LogLevel.DEBUG, 'Added %s to database' % person)
+            else:
+                log.log(logger.LogLevel.ERROR, 'Failed to add %s to database' % person)
+            break
+        else:
+            log.log(logger.LogLevel.WARNING, 'Failed to follow: %s' % person)
+
+
+def unfollow(log, setting, db, twit):
+    """ Unfollows a person """
+    endDate = datetime.datetime.now() - setting.unfollowPersonAfter
+    personList = db.query_fetchall(query.QUERY_GET_FOLLOWS_UPDATE_QUEUE(), (endDate, ))
+    for person in personList:
+        screenName = person[0]
+        unfollowed = twit.unfollow_by_name(screenName)
+        if unfollowed:
+            unfollow_in_db(log, db, screenName)
+        else:
+            res = twit.get_user_search(screenName)
+            if res is False:
+                unfollow_in_db(log, db, screenName)
+            else:
+                log.log(logger.LogLevel.ERROR, 'Failed to unfollow a person that exists: %s' % screenName)
+
+
 def unfollow_in_db(log, db, screenName):
+    """ Updates db to unfollow a person """
     dbResult = db.query_commit(query.QUERY_UPDATE_FOLLOWS(), (screenName, ))
     if dbResult:
         log.log(logger.LogLevel.DEBUG, 'Updated database: %s unfollowed successfully' % screenName)
     else:
         log.log(logger.LogLevel.ERROR, 'Unable to update followingNow status on person: %s' % screenName)
 
-def get_person(db, personList, setting):
+def get_valid_persons(personList, setting):
+    """ returns a list of persons that fulfill the requirements set by settings. """
+    persons = []
     for person in personList:
-        if db.query_exists(query.QUERY_GET_FOLLOWS(), (person.screenName, )):
-            continue
-        
-        validStats = person_valid_stats(person, setting)
-        if validStats:
-            return person
-    return None
+        if person_valid_stats(person, setting):
+            persons.append(person.screenName)
+    return set(persons)
 
 def person_valid_stats(ta, setting):
-    """ checks if the person fits statistics criteria """
+    """ checks if the person fits statistics criteria. ta = TwitterAccount """
     if ta.tweets < setting.followTweetsMin or ta.tweets > setting.followTweetsMax:
         return False
     if ta.friends < setting.followFriendsMin or ta.friends > setting.followFriendsMax:
@@ -96,6 +114,7 @@ def person_valid_stats(ta, setting):
     return True
 
 def get_person_list(r):
+    """ Returns a list of persons, based on a twitter search """
     personList = []
     for status in r.json()['statuses']:
         screen_name = status['user']['screen_name']
@@ -105,7 +124,7 @@ def get_person_list(r):
         statuses_count = status['user']['statuses_count']
         person = TwitterAccount(screen_name, followers, friends_count, favourites_count, statuses_count)
         personList.append(person)
-    return set(personList)
+    return personList
 
 def get_search_q(setting):
     """ returns a random hashtag """
